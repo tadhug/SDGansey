dbdc366bb91e810aac67919349ceaeec4de4f3d5
Ping Luarn <ping.luarn@go.shoreline.edu>
2019-10-20 12:27:56 -0700

    fixed report card

--- parser.js
+++ parser.js
@@ -60,10 +60,6 @@ function update(){
       if(i==14)continue;
         var str = "score_sdg"+i;
         //html +="<p>"+goals[str]+": "+obj[document.getElementById('input').value][str]+"</p>";
-        if(i%4 == 0) {
-          html+='<section class="ftco-section ftco-agent"><div class="container"><div class="row">'
-        }
-        html+='<div class="col-md-3"><div class="agent"><div class="img"><img src="images/Goals'+str+'.png" class="img-fluid" alt="Colorlib Template"></div>'
         html+='<div class="desc"><h3><a href="properties.html">James Stallon</a></h3><p class="h-info"><span class="location">Listing</span> <span class="details">&mdash; 10 Properties</span></p></div></div></div>'
         html+="<tr><td>"+goals[str]+"</td><td>"+obj[document.getElementById('input').value.toLowerCase()][str]+"</td><td>";
         if(obj[document.getElementById('input').value.toLowerCase()][str] > 90) html+="A";
@@ -71,9 +67,6 @@ function update(){
         else if(obj[document.getElementById('input').value.toLowerCase()][str] > 70) html+="C";
         else if(obj[document.getElementById('input').value.toLowerCase()][str] > 60) html+="D";
         else html+="F";
-        if (i%4 == 0) {
-          html+='</div></div></section>'
-        }
         html+="</td></tr>";
     }
 
